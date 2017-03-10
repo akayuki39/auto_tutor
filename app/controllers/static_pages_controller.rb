f0ed343c7f0f19d7299ead908641b94323263f3e
akayuki39 <593740901@qq.com>
2017-03-10 11:19:36 +0800

    Change home

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+  	# if !logged_in?
+  	# 	redirect_to login_path
+  	# else
+  		render 'home'
+  	# end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+	
+	#根据所在页面返回完整标题
+	def full_title(page_title = '')
+		base_title = "AutoTutor"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+	end
 end
